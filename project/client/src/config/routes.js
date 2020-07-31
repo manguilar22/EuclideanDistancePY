126f3a59892dc9b68b7c83e6ee2df086bb276e71
manguilar22 <manguilar22@gmail.com>
2020-07-30 18:56:05 -0600

    routes update@Thu Jul 30 18:56:05 MDT 2020

--- project/client/src/config/routes.js
+++ project/client/src/config/routes.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const url = process.env.REACT_APP_HOSTNAME || "127.0.0.1:8000";
 
+const instance = axios.create({
+    baseURL: `http://${url}`
+});
+
 export const routes = {
-	home: axios.get(`http://${url}/home`),
-    hospitals: axios.get(`http://${url}/hospitals`)
+	home: instance.get(`/home`),
+    hospitals: instance.get(`/hospitals`)
 }
